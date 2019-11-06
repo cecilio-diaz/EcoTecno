ba356b56a74f75359fa7fe0faf8b093f605ec43f
Cristian Baldi <bld.cris.96@gmail.com>
2019-11-06 09:37:58 +0100

    Add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+v-app
+  v-app-bar(app color='primary' dark)
+    .d-flex.align-center
+      v-toolbar-title EcoTecno Idea
+    v-spacer
+    // v-btn(href='https://github.com/vuetifyjs/vuetify/releases/latest' target='_blank' text)
+      span.mr-2 Latest Release
+      v-icon mdi-open-in-new
+  v-content
+    router-view
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  v-layout(wrap)
+    v-flex.pa-3(xs2 style="background-color:white;")
+      .headline.pb-3 Products...
+      v-hover(v-slot:default="{ hover }" v-for="product in products" :key="product.it")
+        v-card.mb-3(:elevation="hover ? 5 : 1")
+          v-img(
+            class="white--text align-end"
+            height="200px"
+            :src="product.image"
+          )
+          v-card-title {{ product.name }}
+          v-card-subtitle {{ product.description }}
+
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+
+  data: () => ({
+    products: [
+      {
+        id: 1,
+        name: "Pomodori",
+        description: "Freschi",
+        image: "https://boygeniusreport.files.wordpress.com/2019/05/capture-2.png?w=782"
+      },
+      {
+        id: 2,
+        name: "Patate",
+        description: "",
+        image: "https://lorenzovinci.it/magazine/wp-content/uploads/2015/04/images_Bacche_di_gioia_patate.1024x768.jpg"
+      },
+      {
+        id: 3,
+        name: "Zucchine",
+        description: "",
+        image: "https://www.salepepe.it/files/2014/12/zucchine.jpg"
+      }
+    ]
+  })
+};
+</script>
